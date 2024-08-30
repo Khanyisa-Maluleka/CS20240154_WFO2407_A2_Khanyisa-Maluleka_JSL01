938236e1ea5cd126819685a0386d9c168748313e
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-08-30 10:09:21 +0200

    Added my second conditional and showed the results

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    
+    let result = ''; 
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (input.startsWith("pet_")) {
-            result = "Valid Syntax"
+        if (input.startsWith('pet_') && /^[a-zA-Z0-9_]+$/.test(input)) {
+            result = 'Valid Syntax'
         }
+        
         else {
-            result = "Invalid Syntax"
+            result = 'Invalid Syntax'
         }
 
             document.getElementById('result').innerText = result;
